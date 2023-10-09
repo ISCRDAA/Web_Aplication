add1f9a84c0553517a4dc42fba0e0d679e28a208
Angel Alonso Rodriguez <55955398+ISCRDAA@users.noreply.github.com>
2023-10-09 11:58:58 -0600

    Pagina web y panel de control

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/style.css">
+    <title>Aleana y Carlup</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="./public/images/logo.jpg" alt="Logo de Tu Tienda de Suéteres">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Suéteres</a></li>
+                <li><a href="#">Ofertas</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="panel.php">Iniciar sesion</a></li>
+                
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Resto de tu contenido de la página -->
+</body>
+</html>
