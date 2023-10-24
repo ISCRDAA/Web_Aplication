7379daec9b923a0cbd33f7c732ddbc28f98b3a4d
Angel Alonso Rodriguez <55955398+ISCRDAA@users.noreply.github.com>
2023-10-24 12:59:50 -0600

    reinicio

--- index.php
+++ index.php
@@ -1,36 +1,11 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
-    <link rel="stylesheet" href="./public/css/style.css">
-
-    
-
-    <title>Aleana y Carlup</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Carlup y Aleana</title>
 </head>
 <body>
-    <header>
-        <div class="logo">
-            <img src="./public/images/logo.jpg" alt="Logo de Tu Tienda de Suéteres">
-        </div>
-        <!-- */Seccion del logo y el menu-->
-        <nav>
-            <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Suéteres</a></li>
-                <li><a href="#">Ofertas</a></li>
-                <li><a href="#">Contacto</a></li>
-                <li><a href="./login/index.php">Iniciar sesion</a></li>
-                
-            </ul>
-        </nav>
-        
-        
-    </header>
-    
-    <!--Slider de productos con mas relevancia -->
-    
+  
 </body>
 </html>
